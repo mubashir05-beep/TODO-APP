716ecc887602573c7fd8cf5f6e47ae4cbec671ef
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2022-11-16 19:50:45 -0800

    React App

---
+++ landingpage/src/App.js
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar";
+function App() {
+  return (
+    <div className="App">
+    {/* Navbar */}
+    <Navabr />
+    {/* Hero */}
+    {/* New Listings */}
+    {/* Take Action */}
+    {/* Stickers */}
+    {/* Related */}
+    {/* Footer */}
+    </div>
+  );
+}
+
+export default App;
