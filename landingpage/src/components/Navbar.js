7b866f7fcbdf1ffefd427434f0872a4b8d06783d
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2022-11-18 12:06:15 -0800

    trying out reacct

--- landingpage/src/components/Navbar.js
+++ landingpage/src/components/Navbar.js
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import { FiMenu } from 'react-icons/fi';
+import logo from '../assets/logo.png'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+    <div className=''>
+        <FiMenu  className='w-12 h-12'/>
+    </div>
+         {/* Left Menu */}
+         {/* Mid Logo */}
+         <div className=''>
+            <img src={logo}/>
+         </div>
+         {/* Right Authentification */}
+    </div>
   )
 }
 
